060e820bd82a3114ed6e9e9413c8cde4244469bb
Robert <robert.jankowski.kontakt@gmail.com>
2020-12-25 16:06:19 +0100

    feat(backend/classes/Game): add draw method | test(beackend/classes/Game): add draw tests

--- backend/classes/Game.js
+++ backend/classes/Game.js
@@ -20,7 +20,7 @@ function Game(id, player, deck) {
             this.pile = [...cards, ...this.pile]
             this.players[playerIndex].hand = this.players[playerIndex].hand.filter(n => !cards.includes(n))
             this.declared = {value: declared, number: cards.length}
-            this.turn = this.turn + 1 < this.players.length - 1 ? this.turn + 1 : 0
+            this.turn = this.turn === this.players.length - 1 ? 0 : this.turn + 1
         }
     }
     this.start = () => {
@@ -30,6 +30,16 @@ function Game(id, player, deck) {
         const firstPlayerIndex = this.players.findIndex(n => n.hand.includes("hearts:2"))
         this.turn = firstPlayerIndex
         this.move(firstPlayerIndex, ["hearts:2"], 2)
+    }
+    this.draw = (playerIndex, number) => {
+        if(number >= this.pile.length) {
+            this.players[playerIndex].hand = [...this.pile.splice(0, this.pile.length - 1), ...this.players[playerIndex].hand]
+            this.pile = [this.pile[this.pile.length - 1]]
+        }
+        else {
+            this.players[playerIndex].hand = [...this.pile.splice(0, number), ...this.players[playerIndex].hand]
+            this.pile = this.pile.splice(0, number + 1)
+        }
     }
 }
 exports.Game = Game
