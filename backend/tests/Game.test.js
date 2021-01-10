bf07db426233547b0fe0971d6bf4ae9958bb77d5
Robert <robert.jankowski.kontakt@gmail.com>
2021-01-10 18:00:56 +0100

    test(backend): remove unnesesarry tests

--- backend/tests/Game.test.js
+++ backend/tests/Game.test.js
@@ -47,63 +47,6 @@ test('Game.join(game started)', () => {
     game.join(player3)
     expect(game.players.length).toEqual(2)
 })
-test('Player.hand(two players/equal values)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    game.join(player2)
-    game.start()
-    expect(game.players.map(n => n.hand)).toEqual([[
-        'spades:8',    'spades:A',
-        'clubs:6',     'diamonds:J',
-        'spades:5',    'hearts:9',
-        'hearts:K',    'diamonds:9',
-        'diamonds:Q',  'spades:Q',
-        'hearts:8',    'clubs:J',
-        'hearts:3',    'hearts:J',
-        'clubs:K',     'diamonds:2',
-        'diamonds:7',  'diamonds:5',
-        'hearts:4',    'spades:4',
-        'diamonds:10', 'spades:9',
-        'spades:3',    'clubs:2',
-        'diamonds:8',  'hearts:7'
-      ],[
-        'spades:J',   'diamonds:6',
-        'clubs:8',    'clubs:7',
-        'hearts:6',   'clubs:10',
-        'hearts:Q',   'hearts:5',
-        'clubs:3',    'clubs:9',
-        'clubs:Q',    'clubs:5',
-        'spades:10',  'diamonds:4',
-        'spades:6',   'diamonds:A',
-        'spades:K',   'diamonds:K',
-        'hearts:A',   'spades:2',
-        'hearts:10',  'diamonds:3',
-        'clubs:A',
-        'spades:7',   'clubs:4'
-      ]])
-})
-test('Player.hand(divisible players/lengths)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    const player3 = new Player("Player3", "mnop")
-    const player4 = new Player("Player4", "vcnm")
-    game.join(player2)
-    game.join(player3)
-    game.join(player4)
-    game.start()
-    expect(game.players.map(n => n.hand.length)).toEqual([13,13,13,12])
-})
-
-test('Player.hand(indivisible players/lengths)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    const player3 = new Player("Player3", "mnop")
-    const player4 = new Player("Player4", "vcnm")
-    const player5 = new Player("Player5", "pcop")
-    game.join(player2)
-    game.join(player3)
-    game.join(player4)
-    game.join(player5)
-    game.start()
-    expect(game.players.map(n => n.hand.length)).toEqual([11,11,9,10,10])
-})
 test('Game.start(correct turn index/last to first index)', () => {
     const player2 = new Player("Player2", "ijkl")
     const player3 = new Player("Player3", "mnop")
@@ -167,17 +110,6 @@ test('Game.move(4 cards)', () => {
     game.move(0, ["spades:3","spades:5", "hearts:3", "diamonds:7"], '3')
     expect(game.pile.length).toEqual(5)
 })
-test('Game.move(invalid number of cards)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    const player3 = new Player("Player3", "mnop")
-    const player4 = new Player("Player4", "vcnm")
-    game.join(player2)
-    game.join(player3)
-    game.join(player4)
-    game.start()
-    game.move(0, ["spades:3","spades:5", "hearts:3", "diamonds:7","clubs:K"], '3')
-    expect(game.pile.length).toEqual(1)
-})
 test('Game.move(game not started)', () => {
     const player2 = new Player("Player2", "ijkl")
     const player3 = new Player("Player3", "mnop")
@@ -187,17 +119,6 @@ test('Game.move(game not started)', () => {
     game.join(player4)
     game.move(0, ["spades:3"], '3')
     expect(game.pile.length).toEqual(0)
-})
-test('Game.move(wrong player turn)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    const player3 = new Player("Player3", "mnop")
-    const player4 = new Player("Player4", "vcnm")
-    game.join(player2)
-    game.join(player3)
-    game.join(player4)
-    game.start()
-    game.move(1, ["spades:3"], '3')
-    expect(game.pile.length).toEqual(1)
 })
 test('Game.move(declared value lower than actual)', () => {
     const player2 = new Player("Player2", "ijkl")
@@ -211,43 +132,6 @@ test('Game.move(declared value lower than actual)', () => {
     game.move(1, ["spades:2"], '2')
     expect(game.pile[0]).toEqual("spades:3")
 })
-test('Game.draw(two cards/values)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    game.join(player2)
-    game.start()
-    game.move(0, ["clubs:2","spades:3"],'3')
-    game.move(1, ["spades:2","diamonds:3"],'4')
-    game.draw(0,2)
-    expect([game.players[0].hand[0], game.players[0].hand[1]]).toEqual(["spades:2","diamonds:3"])
-})
-test('Game.draw(three cards/length)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    game.join(player2)
-    game.start()
-    game.move(0, ["clubs:2","spades:3"],'3')
-    game.move(1, ["spades:2","diamonds:3"],'4')
-    game.draw(0,3)
-    expect([game.pile.length, game.players[0].hand.length]).toEqual([2,27])
-})
-test('Game.draw(more than stack length/length)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    game.join(player2)
-    game.start()
-    game.move(0, ["clubs:2","spades:3"],'3')
-    game.move(1, ["spades:2","diamonds:3"],'4')
-    game.draw(0,10)
-    expect([game.pile.length, game.players[0].hand.length]).toEqual([1, 28])
-})
-test('Game.check(lying)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    game.join(player2)
-    game.start()
-    game.move(0, ["clubs:2","spades:3","diamonds:2","diamonds:5"],'5')
-    game.move(1, ["spades:2","clubs:3","diamonds:3"],'6')
-    game.move(0, ["diamonds:J"],'K')
-    game.check(1)
-    expect(game.players[0].hand.length).toEqual(29)
-})
 test('Game.check(not lying)', () => {
     const player2 = new Player("Player2", "ijkl")
     game.join(player2)
@@ -257,22 +141,6 @@ test('Game.check(not lying)', () => {
     game.move(0, ["diamonds:J"],'J')
     game.check(1)
     expect(game.players[1].hand.length).toEqual(30)
-})
-test('Game.check(lying, other player check)', () => {
-    const player2 = new Player("Player2", "ijkl")
-    const player3 = new Player("Player3", "mnop")
-    const player4 = new Player("Player4", "vcnm")
-    const player5 = new Player("Player5", "pcop")
-    game.join(player2)
-    game.join(player3)
-    game.join(player4)
-    game.join(player5)
-    game.start()
-    game.move(3, ['diamonds:6', 'spades:5'], '5')
-    game.move(4, ['clubs:6', 'hearts:6'], '6')
-    game.move(0, ['hearts:7'],'8')
-    game.check(3)
-    expect(game.players[0].hand.length).toEqual(15)
 })
 test('Game.check(not lying, other player check)', () => {
     const player2 = new Player("Player2", "ijkl")
